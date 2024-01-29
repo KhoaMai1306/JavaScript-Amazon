8a085cf2078c447199b1fa099be562fad58d409c
Khoa Mai <97200413+KhoaMai1306@users.noreply.github.com>
2024-01-29 16:38:23 -0600

    Arrays and Loops with todoList projects

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,59 @@
+const todoList=[
+    {name:'make dinner'
+    ,dueDate: '2024-1-29'
+    },
+    {name:'wash dishes',
+     dueDate:'2024-1-30'
+    }
+];
+//this function shows the list of todo stuffs we had so far
+function renderTodoList(){
+    let todoListHTML ='';
+
+    //generating HTML in Js
+    for(let i=0;i<todoList.length;i++){
+        const todoObject =todoList[i];
+        //const name= todoObject.name; 
+        //const dueDate =todoObject.dueDate;
+        const {name, dueDate}= todoObject;
+
+
+        //we can assign an object in js as an element in html using ``
+        //within `` , we can work it as multi-type value, if we wanna get an object or variable, use ${}
+        //it was together but i separate them into 3 div so we can use grid column ccs
+        const html= `<div>
+        ${name} </div>
+
+        <div> ${dueDate} </div>
+
+        <button onClick="
+            todoList.splice(${i},1);
+            renderTodoList();
+        " class="delete-todo-buttom">Delete</button>
+        
+        
+        `;
+        todoListHTML+=html;
+    }
+    //query helps access html element to JS, .innerHTML access the thing inside that element (after the first <>....)
+    //this part shows the whole list of what we had so far
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo(){
+    const inputElement = document.querySelector('.js-name-input');
+    const dateInputElement =document.querySelector('.js-due-date-input');
+
+    const name= inputElement.value;
+    const dueDate= dateInputElement.value;
+    todoList.push({
+        //name:name,
+        //dueDate :dueDate
+        name,
+        dueDate,
+    });
+
+    inputElement.value=' ';
+    //after each add, we display a new list
+    renderTodoList();
+}
